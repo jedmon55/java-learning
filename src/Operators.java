d4d162bc4d4d0efad623b066d33e296697b680c0
jeffreydowds <jeffreydowds@localhost>
2020-01-31 16:37:45 -0500

    Thrid checkin from STS

--- src/Operators.java
+++ src/Operators.java
@@ -2,14 +2,10 @@
 public class Operators {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	// Add an Interger
+		// Add an Interger
 		 int OperMyInt = 10; // Add integer demo
 		 OperMyInt += 5;     // add 5 to OperMyInt
 		    System.out.println(OperMyInt); // Outputs 15	
-	
-	
 	}
 
 }
