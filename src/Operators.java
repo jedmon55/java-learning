9954492d7139e8adc00fcb1fe72684c5a38a9afa
jeffreydowds <jeffreydowds@localhost>
2020-01-31 16:38:33 -0500

    Fourth from STS

--- src/Operators.java
+++ src/Operators.java
@@ -2,7 +2,7 @@
 public class Operators {
 
 	public static void main(String[] args) {
-		// Add an Interger
+			// Add an Interger
 		 int OperMyInt = 10; // Add integer demo
 		 OperMyInt += 5;     // add 5 to OperMyInt
 		    System.out.println(OperMyInt); // Outputs 15	
